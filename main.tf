9f589f2d920299181417754eaa6af010946200f7
Murthy <murthychiluka@gmail.com>
2023-03-22 21:29:01 +0530

    d

--- main.tf
+++ main.tf
@@ -14,6 +14,14 @@ resource "aws_rds_cluster" "main" {
     { Name = "${var.env}-subnet-group" }
   )
 }
+resource "aws_rds_cluster_instance" "cluster_instances" {
+  count              = var.no_of_instances
+  identifier         = "${var.env}-rds-${count.index}"
+  cluster_identifier = aws_rds_cluster.main.id
+  instance_class     = var.instance_class
+  engine             = var.engine
+  engine_version     = var.engine_version
+}
 
 resource "aws_db_subnet_group" "main" {
   name       = "${var.env}-rds"
